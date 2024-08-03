d8048fbdf3aa63b9c520a734915db63c861a9e12
SuXY <SuXYMC@163.com>
2024-08-03 22:14:45 +0800

    Update to a more fish-like model.

--- FishModel.py
+++ FishModel.py
@@ -1,12 +1,10 @@
+from matplotlib.backend_bases import MouseButton
 from matplotlib import pyplot as plt
-from random import sample
-from numpy import sqrt, pi
-from math import sin, cos
-from numpy.random import random as rand
-from matplotlib.backend_bases import MouseButton as pltmouse
+from random import sample, random
+from math import sqrt, tanh, sin, cos, pi
 
-class dot:
-    def __init__(self, x, y):
+class pnt:
+    def __init__(self, x = 0.0, y = 0.0):
         self.x = x
         self.y = y
 
@@ -15,43 +13,50 @@ class lim:
         self.l = l
         self.u = u
 
-SWARMSIZE = 128
-SPEED = 0.5
-LOCALRANGE = 1.0
-TIGHTRANGE = 0.25
-BOUNDRANGE = 4.0
-INITRANGEFRAC = 4
+SWARMSIZE = 64
+SPEED = 0.2
+ATTRSPEED = 0.3
+INITRANGEFRAC = 1
 
-WEIGHTSELF = 5.0
-WEIGHTLOCAL = 3.0
-WEIGHTRAND = 2.0
-WEIGHTATTRACTOR = 32.0
-WEIGHTCENTER = 1.0
-WEIGHTTIGHT = 20.0
-WEIGHTBOUND = 10.0
+localrange = 1.5
+TIGHTRANGE = 0.8
+BOUNDRANGE = 2.0
 
-TICKRATE = 0.025
-XLIM = lim(-16, 16)
-YLIM = lim(-16, 16)
-ATTRACTORCOUNT = 1
-attrcenter = [[0, 0]]
-attrr = [10.0]
-attrstep = pi / 128
+WEIGHTLOCALCENTER = 1.5
+WEIGHTLOCALVECTOR = 1.0
+WEIGHTSELFVECTOR = 1.5
+WEIGHTLOCALTIGHT = -3.0
+WEIGHTRAND = 3.0
+WEIGHTATTR = 0.5
 
-# Swarm dots
+RANDTURNFRAC = 1 / 8
+ATTRRANDTURNFRAC = 1 / 2
+ATTRBOUNDRAD = 0.5
+ATTRMAXRAD = 4.0
+
+TICKRATE = 0.0125
+XLIM = lim(-8, 8)
+YLIM = lim(-8, 8)
+
+RECTIFYFUNCTION = lambda x: tanh(x) * 2
+
+# Swarm pnts
 swarm = []
 # Swarm vectors
 swarmV = []
-# Attractors
-attr = [dot(4, 0)]
-attrT = []
+# Attractor
+attr = pnt()
+attrV = pnt()
 
-# Interaction control
-moveattr = False
+# Options
+# Plot local for every fish
+#TEST: This might greatly reduce performance
+localplot = False
+enableattr = False
 
 # Gen rand positive or negative (1 or -1)
 def randpn():
-    return rand() * sample([1, -1], k = 1)[0]
+    return random() * sample([1, -1], k = 1)[0]
 
 # Euclidian distance (2D)
 def euclidDist(x0, y0, x1, y1):
@@ -59,165 +64,231 @@ def euclidDist(x0, y0, x1, y1):
 
 # Update single fish
 def updateFish(ind):
-    d = 0.0
-    ld = []
-    lx = []
-    ly = []
-    lvx = []
-    lvy = []
-    tvx = 0.0
-    tvy = 0.0
-    weights = 0.0
+    #TEST: Experimental random localrange
+    #localrange = random() * 2
+    localswm = []
+    localvec = []
+    localdist = []
+    tightswm = []
+    tightdist = []
+    localcnt = 0
+    tightcnt = 0
 
-    # Find local fishes
+    # Get local
     for i in range(SWARMSIZE):
+        # skip self
         if i == ind:
             continue
-        if d := euclidDist(swarm[ind].x, swarm[ind].y, swarm[i].x, swarm[i].y) <= LOCALRANGE:
-            ld.append(d)
-            lx.append(swarm[i].x)
-            ly.append(swarm[i].y)
-            lvx.append(swarmV[i].x)
-            lvy.append(swarmV[i].y)
-    # Find local TIGHT attrs
-    for i in range(ATTRACTORCOUNT):
-        if euclidDist(swarm[ind].x, swarm[ind].y, attr[i].x, attr[i].y) <= TIGHTRANGE:
-            tvx += -1 * (swarm[ind].x + attr[i].x) / 2 * WEIGHTTIGHT
-            tvy += -1 * (swarm[ind].y + attr[i].y) / 2 * WEIGHTTIGHT
-            weights += WEIGHTTIGHT
+        if d := euclidDist(swarm[ind].x, swarm[ind].y, swarm[i].x, swarm[i].y) <= localrange:
+            localswm.append(swarm[i])
+            localvec.append(swarmV[i])
+            localdist.append(d)
+            localcnt += 1
+            if d <= TIGHTRANGE:
+                tightswm.append(swarm[i])
+                tightdist.append(d)
+                tightcnt += 1
 
-    # Weighted average
+    # Plot local
+    if localplot == True:
+        localx_vals = [pnt.x for pnt in localswm]
+        localy_vals = [pnt.y for pnt in localswm]
+        plt.plot(localx_vals, localy_vals, 'b.-')
 
+    # Calc local stuff
+    # local center point
+    centpt = pnt()
+    # local average vector
+    avgvec = pnt()
+    # relative vector to local center
+    relcentvec = pnt()
+    # tight center point
+    tightpt = pnt()
+    # relative vector to tight center
+    reltightvec = pnt()
     # local
-    for i, j in zip(lvx, lvy):
-        tvx += i * WEIGHTLOCAL
-        tvy += j * WEIGHTLOCAL
-        weights += WEIGHTLOCAL
-    # self
-    tvx += swarmV[ind].x * WEIGHTSELF
-    tvy += swarmV[ind].y * WEIGHTSELF
-    weights += WEIGHTSELF
-    # attr
-    for i in range(ATTRACTORCOUNT):
-        tvx += (attr[i].x - swarm[ind].x) / euclidDist(swarm[ind].x, swarm[ind].y, attr[i].x, attr[i].y) * WEIGHTATTRACTOR
-        tvy += (attr[i].y - swarm[ind].y) / euclidDist(swarm[ind].x, swarm[ind].y, attr[i].x, attr[i].y) * WEIGHTATTRACTOR
-        weights += WEIGHTATTRACTOR
-    # local center
-    lxs = 0
-    lys = 0
-    xylen = 0
-    for i, j in zip(lx, ly):
-        lxs += i
-        lys += j
-        xylen += 1
-    if xylen != 0:
-        lxs /= xylen
-        lys /= xylen
-        if euclidDist(swarm[ind].x, swarm[ind].y, lxs, lys) <= TIGHTRANGE:
-            # tight
-            tvx += -1 * (lxs - swarm[ind].x) / euclidDist(swarm[ind].x, swarm[ind].y, lxs, lys) * WEIGHTATTRACTOR
-            tvy += -1 * (lys - swarm[ind].y) / euclidDist(swarm[ind].x, swarm[ind].y, lxs, lys) * WEIGHTATTRACTOR
-        else:
-            # center
-            tvx += (lxs - swarm[ind].x) / euclidDist(swarm[ind].x, swarm[ind].y, lxs, lys) * WEIGHTATTRACTOR
-            tvy += (lys - swarm[ind].y) / euclidDist(swarm[ind].x, swarm[ind].y, lxs, lys) * WEIGHTATTRACTOR
-            weights += WEIGHTCENTER
+    if localcnt > 0:
+        for i in range(localcnt):
+            centpt.x += localswm[i].x * localdist[i]
+            centpt.y += localswm[i].y * localdist[i]
+            avgvec.x += localvec[i].x
+            avgvec.y += localvec[i].y
+        centpt.x /= localcnt
+        centpt.y /= localcnt
+        avgvec.x /= localcnt
+        avgvec.y /= localcnt
+        relcentvec.x = (centpt.x - swarm[ind].x)
+        relcentvec.y = (centpt.y - swarm[ind].y)
+    # tight
+    if tightcnt > 0:
+        for i in range(tightcnt):
+            tightpt.x += tightswm[i].x * 1 / tightdist[i]
+            tightpt.y += tightswm[i].y * 1 / tightdist[i]
+        tightpt.x /= tightcnt
+        tightpt.y /= tightcnt
+        reltightvec.x = (tightpt.x - swarm[ind].x)
+        reltightvec.y = (tightpt.y - swarm[ind].y)
 
-    if weights != 0:
-        tvx /= weights
-        tvy /= weights
+    # Attractor force
+    attrdist = euclidDist(swarm[ind].x, swarm[ind].y, attr.x, attr.y)
+    attrvec = pnt()
+    if attrdist != 0:
+        if attrdist <= ATTRBOUNDRAD:
+            # distract
+            attrvec.x = -(attr.x - swarm[ind].x) / attrdist
+            attrvec.y = -(attr.y - swarm[ind].y) / attrdist
+        elif attrdist >= ATTRMAXRAD:
+            # attract
+            attrvec.x = (attr.x - swarm[ind].x) * attrdist
+            attrvec.y = (attr.y - swarm[ind].y) * attrdist
 
-    # Give value
-    swarmV[ind].x = tvx
-    swarmV[ind].y = tvy
-    swarmV[ind].x += randpn() * WEIGHTRAND
-    swarmV[ind].y += randpn() * WEIGHTRAND
+    # Random vector
+    randvec = pnt(tanh(randpn()), tanh(randpn()))
+
+    # Adjust own vector
+    weightsum = 0.0
+    swarmV[ind].x *= WEIGHTSELFVECTOR
+    swarmV[ind].y *= WEIGHTSELFVECTOR
+    swarmV[ind].x += relcentvec.x * WEIGHTLOCALCENTER
+    swarmV[ind].y += relcentvec.y * WEIGHTLOCALCENTER
+    swarmV[ind].x += avgvec.x * WEIGHTLOCALVECTOR
+    swarmV[ind].y += avgvec.y * WEIGHTLOCALVECTOR
+    swarmV[ind].x += reltightvec.x * WEIGHTLOCALTIGHT
+    swarmV[ind].y += reltightvec.y * WEIGHTLOCALTIGHT
+    swarmV[ind].x += randvec.x * WEIGHTRAND
+    swarmV[ind].y += randvec.y * WEIGHTRAND
+    weightsum += WEIGHTSELFVECTOR + WEIGHTLOCALCENTER + WEIGHTLOCALVECTOR + WEIGHTLOCALTIGHT
+    if enableattr == True:
+        swarmV[ind].x += attrvec.x * WEIGHTATTR
+        swarmV[ind].y += attrvec.y * WEIGHTATTR
+        weightsum += WEIGHTATTR
+    # sub to weighted sum
+    if weightsum > 0:
+        swarmV[ind].x /= weightsum
+        swarmV[ind].y /= weightsum
+
+    # Vector rectify
+    swarmV[ind].x = RECTIFYFUNCTION(swarmV[ind].x)
+    swarmV[ind].y = RECTIFYFUNCTION(swarmV[ind].y)
+
+    # Vector turn
+    thetax = random() * ATTRRANDTURNFRAC
+    thetay = random() * ATTRRANDTURNFRAC
+    vx1 = swarmV[ind].x * cos(thetax) - swarmV[ind].y * sin(thetax)
+    vy1 = swarmV[ind].x * sin(thetay) + swarmV[ind].y * cos(thetay)
+    swarmV[ind].x = vx1
+    swarmV[ind].y = vy1
 
     # Move
     swarm[ind].x += swarmV[ind].x * SPEED
     swarm[ind].y += swarmV[ind].y * SPEED
-    # Near bound: turn back
-    if swarm[ind].x < XLIM.l + BOUNDRANGE or swarm[ind].x > XLIM.u - BOUNDRANGE:
-        swarmV[ind].x *= -1 * WEIGHTBOUND
-    if swarm[ind].y < YLIM.l + BOUNDRANGE or swarm[ind].y > YLIM.u - BOUNDRANGE:
-        swarmV[ind].y *= -1 * WEIGHTBOUND
 
 # Update whole swarm
 def updateSwarm():
     for ind in range(SWARMSIZE):
-        # Out of bound: rectify
+        # Out of bound: reverse coords (finite universe)
+        # -
         if swarm[ind].x < XLIM.l:
-            swarm[ind].x = XLIM.l + BOUNDRANGE
-        elif swarm[ind].y < YLIM.l:
-            swarm[ind].y = YLIM.l + BOUNDRANGE
-        elif swarm[ind].x > XLIM.u:
-            swarm[ind].x = XLIM.u - BOUNDRANGE
-        elif swarm[ind].y > YLIM.u:
-            swarm[ind].y = YLIM.u - BOUNDRANGE
-        # vector
-        if swarmV[ind].x < XLIM.l:
-            swarmV[ind].x = XLIM.l
-        elif swarmV[ind].y < YLIM.l:
-            swarmV[ind].y = YLIM.l
-        elif swarmV[ind].x > XLIM.u:
-            swarmV[ind].x = XLIM.u
-        elif swarmV[ind].y > YLIM.u:
-            swarmV[ind].y = YLIM.u
-    for i in range(SWARMSIZE):
-        updateFish(i)
+            swarm[ind].x = XLIM.u
+        if swarm[ind].y < YLIM.l:
+            swarm[ind].y = YLIM.u
+        # +
+        if swarm[ind].x > XLIM.u:
+            swarm[ind].x = XLIM.l
+        if swarm[ind].y > YLIM.u:
+            swarm[ind].y = YLIM.l
+        updateFish(ind)
 
-def spinAttractor(ind):
-    attrT[ind] += attrstep + rand() / 16
-    attrr[ind] += randpn() / 8
-    if attrT[ind] >= 2 * pi:
-        attrT[ind] %= 2 * pi
-    attr[ind].x = cos(attrT[ind]) * attrr[ind] + randpn() / 8
-    attr[ind].y = sin(attrT[ind]) * attrr[ind] + randpn() / 8
+# Update attractor
+#WARN: Might not adapt to new XLIM and YLIMs
+def updateAttr():
+    # Vector turn
+    theta = random() * ATTRRANDTURNFRAC
+    vx1 = attrV.x * cos(theta) - attrV.y * sin(theta)
+    vy1 = attrV.x * sin(theta) + attrV.y * cos(theta)
 
-def on_move(event):
-    if event.inaxes:
-        global attr
-        if moveattr == True:
-            attr[0].x = event.xdata
-            attr[0].y = event.ydata
+    # Center vector middle point
+    cent = pnt(XLIM.u + XLIM.l, YLIM.u + YLIM.l)
+    if euclidDist(attr.x, attr.y, cent.x, cent.y) > XLIM.u * 7 / 8:
+        '''
+        relcentvec = pnt()
+        relcentvec.x = attr.x - cent.x
+        relcentvec.y = attr.y - cent.x
+        vx1 = (vx1 + relcentvec.x * 1 / 2) / 2
+        vy1 = (vy1 + relcentvec.y * 1 / 2) / 2
+        '''
+        theta = pi / 3
+        vx1 += attrV.x * cos(theta) - attrV.y * sin(theta)
+        vy1 += attrV.x * sin(theta) + attrV.y * cos(theta)
 
+    attrV.x = vx1
+    attrV.y = vy1
+
+    # Vector rectify
+    attrV.x = RECTIFYFUNCTION(attrV.x)
+    attrV.y = RECTIFYFUNCTION(attrV.y)
+
+    # Move
+    attr.x += attrV.x * ATTRSPEED
+    attr.y += attrV.y * ATTRSPEED
+
+    # Out of bound: reverse coords (finite universe)
+    SHIFT = random() * 4.0
+    # -
+    if attr.x < XLIM.l:
+        attr.x = XLIM.u - SHIFT
+    if attr.y < YLIM.l:
+        attr.y = YLIM.u - SHIFT
+    # +
+    if attr.x > XLIM.u:
+        attr.x = XLIM.l + SHIFT
+    if attr.y > YLIM.u:
+        attr.y = YLIM.l + SHIFT
+
+# Click event
 def on_click(event):
-    if event.button is pltmouse.LEFT:
-        # toggle moveattr
-        global moveattr
-        if moveattr == False:
-            moveattr = True
-        else:
-            moveattr = False
+    if event.button is MouseButton.LEFT:
+        # Toggle plot local
+        global localplot
+        localplot = not localplot
+    elif event.button is MouseButton.RIGHT:
+        # Toggle attractor force
+        global enableattr
+        enableattr = not enableattr
 
 def main():
     # Init fishes
+    #TEST: Might correct vector
+    vecmax = sqrt(1 / 2)
     for i in range(SWARMSIZE):
-        swarm.append(dot(XLIM.u * randpn() / INITRANGEFRAC, YLIM.u * randpn() / INITRANGEFRAC))
-        swarmV.append(dot(randpn(), randpn()))
-    for i in range(ATTRACTORCOUNT):
-        attrT.append(0.0)
+        swarm.append(pnt(XLIM.u * randpn() / INITRANGEFRAC, YLIM.u * randpn() / INITRANGEFRAC))
+        swarmV.append(pnt(randpn() * vecmax, randpn() * vecmax))
+    attr.x = randpn()
+    attr.y = randpn()
+    attrV.x = randpn()
+    attrV.y = randpn()
+
+    plt.connect('button_press_event', on_click) 
 
     # Draw plot
     while True:
-        #HACK: This does not support mutiple attractors
-        binding_id = plt.connect('motion_notify_event', on_move)
-        plt.connect('button_press_event', on_click)
-
         plt.cla()
         updateSwarm()
-        if moveattr == False:
-            spinAttractor(0)
+        if enableattr == True:
+            updateAttr()
         plt.title('Swarm Simulation')
         plt.xlim(XLIM.l, XLIM.u)
         plt.ylim(YLIM.l, YLIM.u)
-        x_vals = [dot.x for dot in swarm]
-        y_vals = [dot.y for dot in swarm]
-        plt.plot(x_vals, y_vals, 'b-')
-        for i in range(ATTRACTORCOUNT):
-            plt.plot(attr[i].x, attr[i].y, 'g.')
+        x_vals = [pnt.x for pnt in swarm]
+        y_vals = [pnt.y for pnt in swarm]
+        plt.xlabel(f'localPlot = {localplot}, attractorForce = {enableattr}')
+        if localplot == False:
+            plt.plot(x_vals, y_vals, 'b^')
+        if enableattr == True:
+            plt.plot(attr.x, attr.y, 'g.')
         plt.draw()
         plt.pause(TICKRATE)
 
 if __name__ == '__main__':
     main()
+
